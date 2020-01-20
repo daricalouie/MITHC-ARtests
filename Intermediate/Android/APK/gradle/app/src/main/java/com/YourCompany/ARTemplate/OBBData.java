09d72dcb9d71844d91b3d0c31ac9b9cabfcd9a69
Darica Louie <darica.louie@gmail.com>
2020-01-20 13:03:05 -0800

    fade function works, bug of delay after init

--- Intermediate/Android/APK/gradle/app/src/main/java/com/YourCompany/ARTemplate/OBBData.java
+++ Intermediate/Android/APK/gradle/app/src/main/java/com/YourCompany/ARTemplate/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-52817893L // the length of the file in bytes
+53065886L // the length of the file in bytes
 )
 };
 };
