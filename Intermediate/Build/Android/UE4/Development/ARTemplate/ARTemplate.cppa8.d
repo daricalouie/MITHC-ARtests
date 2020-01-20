bff083a54bac48cfe3761a9fe9e60f5a206cf5d6
Darica Louie <darica.louie@gmail.com>
2020-01-20 13:24:10 -0800

    fade function works, bug of delay after init

--- Intermediate/Build/Android/UE4/Development/ARTemplate/ARTemplate.cppa8.d
+++
@@ -1,4 +0,0 @@
-C:/Users/Darica/Documents/Unreal\ Projects/ARTemplate/Intermediate/Build/Android/UE4/Development/ARTemplate/ARTemplate.cppa8.o: \
-  C:/Users/Darica/Documents/Unreal\ Projects/ARTemplate/Intermediate/Source/ARTemplate.cpp \
-  C:/Users/Darica/Documents/Unreal\ Projects/ARTemplate/Intermediate/Build/Android/ARTemplate/Development/Core/SharedPCH.Core.h \
-  C:/Users/Darica/Documents/Unreal\ Projects/ARTemplate/Intermediate/Build/Android/UE4/Development/ARTemplate/Definitions.ARTemplate.h
